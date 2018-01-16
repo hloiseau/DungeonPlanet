27a16f391f93ed231752b62fc34a7f6bea8aec19
melodie <mly@intechinfo.fr>
2018-01-16 09:17:09 +0100

    Add some dialog

--- DungeonPlanet/DungeonPlanet.Library/NPCDialogLib.cs
+++ DungeonPlanet/DungeonPlanet.Library/NPCDialogLib.cs
@@ -10,40 +10,40 @@ namespace DungeonPlanet.Library
     {
         Random _random;
         int _cursor;
-        List<String> _wiseLign;
-        List<String> _chroniclerLign;
+        List<String> _wiseLine;
+        List<String> _chroniclerLine;
 
         public NPCDialogLib()
         {
             _random = new Random();
             _cursor = 0;
-            _wiseLign = new List<string>();
-            _chroniclerLign = new List<string>();
+            _wiseLine = new List<string>();
+            _chroniclerLine = new List<string>();
         }
 
         public string ChooseSentenceForWise()
         {
-            _wiseLign.Add("Connaissez vous le jeune Clyde Vanilla ? il est tres connu lui et ses compagnons, menes par le magnifique sergent Moustachios.");
-            _wiseLign.Add("Allumer le feux !!! hey, tu sais tu peux acheter des munitions enflammees chez l armurier.");
-            _wiseLign.Add("I play DonjonPlanet every day I play Donjon...  Ho ho ho j adore cette chanson !");
-            _wiseLign.Add("On m appelle l ovni tatata... J ai cette musique dans la tete depuis plus d'une heure et je ne l aime meme pas !");
-            _wiseLign.Add("Un vieux proverbe dit que si vous nous notez bien vous aurez des potins. Noter qui ? J'en sais rien moi !");
-            _wiseLign.Add("C etait mieux avant !");
-            _wiseLign.Add("SEUL LINK PEUT VAINCRE GANON !! ... Au fait c'est qui ganon ?");
+            _wiseLine.Add("Connaissez vous le jeune Clyde Vanilla ? il est tres connu lui et ses compagnons, menes par le magnifique sergent Moustachios.");
+            _wiseLine.Add("Allumer le feux !!! hey, tu sais tu peux acheter des munitions enflammees chez l armurier.");
+            _wiseLine.Add("I play DonjonPlanet every day I play Donjon...  Ho ho ho j adore cette chanson !");
+            _wiseLine.Add("On m appelle l ovni tatata... J ai cette musique dans la tete depuis plus d'une heure et je ne l aime meme pas !");
+            _wiseLine.Add("Un vieux proverbe dit que si vous nous notez bien vous aurez des potins. Noter qui ? J'en sais rien moi !");
+            _wiseLine.Add("C etait mieux avant !");
+            _wiseLine.Add("SEUL LINK PEUT VAINCRE GANON !! ... Au fait c'est qui ganon ?");
+            _wiseLine.Add("Cette planete est casse-pied pour parler. On peux pas utiliser d accents et d aspostrophes ! ");
 
-            int Numberlign = RandomNumber(_wiseLign.Count);
+            int Numberlign = RandomNumber(_wiseLine.Count);
 
-            return _wiseLign[Numberlign];
+            return _wiseLine[Numberlign];
         }
 
         public string ChooseSentenceForChronicler()
         {
-            _chroniclerLign.Add("Bonjour chere scientifique ! Que puis-je pour vous aujourd hui ?");
-            _chroniclerLign.Add("2");
-            _chroniclerLign.Add("3");
-            _chroniclerLign.Add("4");
+            _chroniclerLine.Add("Bonjour chere scientifique ! Que puis-je pour vous aujourd hui ?");
+            _chroniclerLine.Add("");
+            _chroniclerLine.Add("");
 
-            string toSay = _chroniclerLign[_cursor];
+            string toSay = _chroniclerLine[_cursor];
 
             _cursor++;
 
